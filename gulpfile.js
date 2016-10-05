ca089126d06ba7176143969d1f7ed8c429156f5f
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2016-10-06 02:39:55 +0300

    Настроена автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,36 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgmin = require("gulp-svgmin");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function () {
     gulp.src("sass/style.scss")
         .pipe(plumber())
         .pipe(sass())
         .pipe(postcss([
-      autoprefixer({
-                browsers: [
+        autoprefixer({
+            browsers: [
         "last 1 version",
         "last 2 Chrome versions",
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]
-            })
+    ]}),
+        mqpacker({
+        sort: true
+        })
     ]))
-        .pipe(gulp.dest("css"))
-        .pipe(server.stream());
+        .pipe(gulp.dest("build/css"))
+        .pipe(server.stream())
+        .pipe(minify())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("build/css"));
 });
 
 gulp.task("serve", ["style"], function () {
@@ -36,4 +48,39 @@ gulp.task("serve", ["style"], function () {
 
     gulp.watch("sass/**/*.{scss,sass}", ["style"]);
     gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("images", function() {
+    return gulp.src("build/img/**/*.{png,jpg,gif}")
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.jpegtran({progressive: true})
+        ]))
+        .pipe(gulp.dest("build/img"));
+    });
+
+gulp.task("svgimages", function() {
+    return gulp.src("build/img/*.svg")
+        .pipe(svgmin())
+        .pipe(gulp.dest("build/img"));
+    });
+
+gulp.task("build", function(fn) {
+    run("clean", "copy", "style", "images", "svgimages", fn);
+})
+
+gulp.task("copy", function() {
+    return gulp.src([
+        "fonts/**/*.{woff,woff2}",
+        "img/**",
+        "js/**",
+        "*.html"
+    ], {
+        base: "."
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+    return del("build");
 });
