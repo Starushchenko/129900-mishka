8e042ca8fdd480ad223cd5363d98f6d874b481b2
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2016-10-09 01:55:33 +0300

    Завершение написания js, небольшие правки отступов

--- js/javascript.js
+++ js/javascript.js
@@ -2,9 +2,22 @@ var buyLink = document.querySelector(".order-button");
 var overlay = document.querySelector(".modal-overlay");
 var modal = document.querySelector(".to-basket-modal");
 var menuButton = document.querySelector(".page-header__menu-toggle");
-var menuButtonOpened = document.querySelector(".page-header__menu-toggle--opened");
 var menu = document.querySelector(".main-nav");
 var mapNojs = document.querySelector(".contacts__map-nojs");
+
+    function hideNojs() {
+        menu.classList.remove("main-nav--nojs");
+        menu.classList.add("main-nav--closed");
+        menuButton.classList.add("page-header__menu-toggle--closed");
+    }
+
+    hideNojs();
+
+    menuButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        menu.classList.remove("main-nav--closed");
+        menuButton.classList.toggle("page-header__menu-toggle--closed");
+    });
 
     buyLink.addEventListener("click", function(event) {
         event.preventDefault();
@@ -18,31 +31,14 @@ var mapNojs = document.querySelector(".contacts__map-nojs");
         overlay.classList.remove("modal-overlay--show");
         });
 
-    function hideNojs() {
-        menu.classList.remove("main-nav--nojs");
-        menu.classList.add("main-nav--closed");
-        menuButton.classList.add("page-header__menu-toggle--closed");
+    function hideMapNojs() {
         mapNojs.classList.add("contacts__map-hide");
     }
 
-    hideNojs();
+    hideMapNojs();
 
 
-    menuButton.addEventListener("click", function(event) {
-        event.preventDefault();
-        menu.classList.remove("main-nav--closed");
-        menu.classList.add("main-nav--opened");
-        menuButton.classList.remove("page-header__menu-toggle--closed");
-        menuButton.classList.add("page-header__menu-toggle--opened");
-        });
 
-    menuButtonOpened.addEventListener("click", function(event) {
-        event.preventDefault();
-        menu.classList.remove("main-nav--opened");
-        menu.classList.add("main-nav--closed");
-        menuButton.classList.remove("page-header__menu-toggle--opened");
-        menuButton.classList.add("page-header__menu-toggle--closed");
-        });
 
 
 
